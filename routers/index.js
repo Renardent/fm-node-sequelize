d607f50e92f2e11c584580e45ad19dc3d80d1d23
Renardent <renard_ardente@gmail.com>
2023-05-31 01:18:46 +0300

    controllers

--- routers/index.js
+++ routers/index.js
@@ -1,6 +1,9 @@
 const {Router} = require('express');
+const UserController =require('../controllers/User.controller');
 
 const router = Router();
-router.get('/user');
+router.post('/user', UserController.createUser);
+router.get('/users', UserController.findAll);
+router.get('/user/:id', UserController.findByPK);
 
 module.exports = router;
