0bc6f2a34b0bab99b95f3c66b92903e6a511a118
Renardent <renard_ardente@gmail.com>
2023-05-30 21:15:14 +0300

    sequelize beginig

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 //////server-code
+
+const http = require('http');
+const app = require('./app');
+const PORT = process.env.PORT || 3000;
+
+const server = http.createServer(app);
+
+server.listen(PORT, () => {
+    console.log(`App started on port ${PORT}`);
+});
